5a77993d93233b3f590e300473f03775515632fa
asalamat <106166576+asalamat@users.noreply.github.com>
2023-05-11 11:36:29 -0400

    Update ad_query.py

--- ad_query.py
+++ ad_query.py
@@ -1,10 +1,16 @@
-from ldap3 import Server, Connection, ALL
+from ldap3 import ALL, Connection, Server
+from ldap3.core.exceptions import LDAPException
 
-server = Server('10.9.11.70', get_info=ALL)
-conn = Connection(server, 'lcbo\ueais', 'M3rryXm@s4225', auto_bind=True)
+username = "lcbo\ueais"
+password = "M3rryXm@s4225"
+ldap_base = "dc=lcbo,dc=com"
 
+server = Server(
+    host="ldaps://dch005.lcbo.com",
+    port=636,
+    use_ssl=True,
+    get_info=ALL,
 conn.search('dc=lcbo,dc=com', '(objectclass=computer)')
-
 computers = [entry['attributes']['dNSHostName'] for entry in conn.entries]
 
 with open('computers.txt', 'w') as f:
