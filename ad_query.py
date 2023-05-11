c72956b6dc6e5b8169e7b7b1fbbdda71a5323d87
asalamat <106166576+asalamat@users.noreply.github.com>
2023-05-11 11:21:32 -0400

    Create ad_query.py

---
+++ ad_query.py
@@ -0,0 +1,12 @@
+from ldap3 import Server, Connection, ALL
+
+server = Server('dch001.lcbo.com', get_info=ALL)
+conn = Connection(server, 'lcbo\ueais', 'M3rryXm@s4225', auto_bind=True)
+
+conn.search('dc=lcbo,dc=com', '(objectclass=computer)')
+
+computers = [entry['attributes']['dNSHostName'] for entry in conn.entries]
+
+with open('computers.txt', 'w') as f:
+    for computer in computers:
+        f.write(computer + '\n')
